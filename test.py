b44993c7fddcc0618b2ab707ca34ee19b4a7aba5
Emilie <emilie.b.holmberg@hotmail.dk>
2024-02-05 11:47:00 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
-import os
-os.remove("TestFile.txt")
+import datetime
+from os.path import expanduser
 
-f = open("/home/ubuntu/Desktop/TestFile.txt", "x")
-f.write("Text")
+f = open(expanduser("~") + '/Desktop/TestFile.txt', 'w')
+f.write("Text\n" + str(datetime.datetime.now()))
 f.close()
